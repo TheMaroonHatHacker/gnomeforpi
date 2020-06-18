7a3587267068b14136677bb70696ccd1278f83c9
TerraGitHuB <64737688+TerraGitHuB@users.noreply.github.com>
2020-06-18 15:19:38 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,22 @@
 echo "Starting script..."
+
 echo "Install Gnome package"
-sudo apt install xorg gnome gnome-shell
+apt install xorg gnome gnome-shell #installs GNOME desktop and xorg onto the system
 wait
+
+echo "Disabling dhcpcd to be able to connect to wifi via gnome network manager"
+systemctl disable dhcpcd && /etc/init.d/dhcpcd stop
+wait
+
+echo "change permission settings for GNOME"
+rm /etc/polkit-1/localauthority.conf.d/
+wait
+cp localauthority.conf.d /etc/polkit-1/localauthority.conf.d
+wait
+
+echo "Install chrome extension intergration for GNOME"
+apt install chrome-gnome-shell
+wait
+
+echo "Rebooting, enjoy your new desktop!"
+reboot
