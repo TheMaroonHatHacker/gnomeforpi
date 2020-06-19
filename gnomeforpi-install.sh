303925b4ba51e5b0ea599543a61d42c5231ff14b
TerraGitHuB <64737688+TerraGitHuB@users.noreply.github.com>
2020-06-19 09:15:18 +0100

    Update gnomeforpi-install.sh

--- gnomeforpi-install.sh
+++ gnomeforpi-install.sh
@@ -15,20 +15,20 @@ sleep 2
 systemctl disable dhcpcd && /etc/init.d/dhcpcd stop
 wait
 
-echo "Correcting Audio for GNOME"
-sleep 2
-rm /etc/pulse/default.pa
-wait
-mv default.pa /etc/pulse/default.pa
-wait
-rm /usr/share/pulseaudio/alsa-mixer/profile-sets/default.conf
-wait
-mv  default.conf /usr/share/pulseaudio/alsa-mixer/profile-sets/default.conf
-echo "change permission settings for GNOME"
-sleep 2
-wait
-cp localauthority.conf /etc/polkit-1/localauthority.conf.d/51-localauthority.conf
-wait
+#echo "Correcting Audio for GNOME"
+#sleep 2
+#rm /etc/pulse/default.pa
+#wait
+#mv default.pa /etc/pulse/default.pa
+#wait
+#rm /usr/share/pulseaudio/alsa-mixer/profile-sets/default.conf
+#wait
+#mv  default.conf /usr/share/pulseaudio/alsa-mixer/profile-sets/default.conf
+#echo "change permission settings for GNOME"
+#sleep 2
+#wait
+#cp localauthority.conf /etc/polkit-1/localauthority.conf.d/51-localauthority.conf
+#wait
 
 echo "Installing chromium"
 sleep 2
