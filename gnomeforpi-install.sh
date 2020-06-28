c5a36e7df25fdc2a898263ab0e8d9a0c24332214
TerraGitHuB <64737688+TerraGitHuB@users.noreply.github.com>
2020-06-28 18:04:27 +0100

    Update gnomeforpi-install.sh

--- gnomeforpi-install.sh
+++ gnomeforpi-install.sh
@@ -9,7 +9,7 @@ wait
 
 echo "Install all packages"
 sleep 2
-apt-get install -y gnome-shell rpi-chromium-mods #installs GNOME desktop and xorg onto the system
+apt-get install -y gnome gnome-shell rpi-chromium-mods xorg #installs all required packges
 wait
 
 echo "Disabling dhcpcd to be able to connect to wifi via gnome network manager"
@@ -17,20 +17,27 @@ sleep 2
 systemctl disable dhcpcd && /etc/init.d/dhcpcd stop
 wait
 
-#echo "Correcting Audio for GNOME"
-#sleep 2
-#rm /etc/pulse/default.pa
-#wait
-#mv default.pa /etc/pulse/default.pa
-#wait
-#rm /usr/share/pulseaudio/alsa-mixer/profile-sets/default.conf
-#wait
-#mv  default.conf /usr/share/pulseaudio/alsa-mixer/profile-sets/default.conf
-#echo "change permission settings for GNOME"
-#sleep 2
-#wait
-#cp localauthority.conf /etc/polkit-1/localauthority.conf.d/51-localauthority.conf
-#wait
+echo "Correcting Audio for GNOME"
+sleep 2
+sudo sed -i 's/load-module module-udev-detect/load-module module-udev-detect tsched=0/g' /etc/pulse/default.pa
+wait
+sudo sed -i 's/[Mapping analog-mono]/;[Mapping analog-mono]/g' /usr/share/pulseaudio/alsa-mixer/profile-sets/default.conf
+wait
+sudo sed -i 's/device-strings = hw:%f/;device-strings = hw:%f/g' /usr/share/pulseaudio/alsa-mixer/profile-sets/default.conf
+wait
+sudo sed -i 's/channel-map = mono/;channel-map = mono/g' /usr/share/pulseaudio/alsa-mixer/profile-sets/default.conf
+wait
+sudo sed -i 's/paths-output = analog-output analog-output-lineout analog-output-speaker analog-output-headphones analog-output-headphones-2 analog-output-mono/;paths-output = analog-output analog-output-lineout analog-output-speaker analog-output-headphones analog-output-headphones-2 analog-output-mono/g' /usr/share/pulseaudio/alsa-mixer/profile-sets/default.conf
+wait
+sudo sed -i 's/paths-input = analog-input-front-mic analog-input-rear-mic analog-input-internal-mic analog-input-dock-mic analog-input analog-input-mic analog-input-linein analog-input-aux analog-input-video analog-input-tvtuner analog-input-fm analog-input-mic-line analog-input-headset-mic/;paths-input = analog-input-front-mic analog-input-rear-mic analog-input-internal-mic analog-input-dock-mic analog-input analog-input-mic analog-input-linein analog-input-aux analog-input-video analog-input-tvtuner analog-input-fm analog-input-mic-line analog-input-headset-mic/g' /usr/share/pulseaudio/alsa-mixer/profile-sets/default.conf
+sudo sed -i 's/priority = 7/;priority = 7/g' /usr/share/pulseaudio/alsa-mixer/profile-sets/default.conf
+
+echo "change permission settings for GNOME"
+sleep 2
+sudo sh -c 'echo "[Configuration]" >> /etc/polkit-1/localauthority.conf.d/51-admin.conf'
+wait
+sudo sh -c 'echo "AdminIdentities=unix-group:sudo;unix-group:admin" >> /etc/polkit-1/localauthority.conf.d/51-admin.conf'
+wait
 
 echo "Rebooting, enjoy your new desktop!"
 sleep 2
